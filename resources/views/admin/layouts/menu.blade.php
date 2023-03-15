3eb797d2382bad67e78696c6afea0e1873c331a8
nayan97 <nayanislam085@gmail.com>
2023-03-15 23:38:56 +0600

    admin page loaded

---
+++ resources/views/admin/layouts/menu.blade.php
@@ -0,0 +1,23 @@
+<div class="sidebar" id="sidebar">
+                <div class="sidebar-inner slimscroll">
+					<div id="sidebar-menu" class="sidebar-menu">
+					<ul>
+							<li class="menu-title"> 
+								<span>Main</span>
+							</li>
+							<li class="active"> 
+								<a href="index.html"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+							</li>
+							
+						
+							<li class="submenu">
+								<a href="#"><i class="fe fe-warning"></i> <span> Error Pages </span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="error-404.html">404 Error </a></li>
+									<li><a href="error-500.html">500 Error </a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+                </div>
+            </div>
---
+++ resources/views/admin/layouts/page-header.blade.php
@@ -0,0 +1,3 @@
+                <!-- Page Header -->
+         
+                <!-- /Page Header -->
